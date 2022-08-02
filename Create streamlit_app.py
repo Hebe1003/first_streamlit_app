92db72e5afd70c41c77b8dd6f367b5c9509cf090
Hebe Li <110219308+Hebe1003@users.noreply.github.com>
2022-08-02 20:56:50 +0800

    Update Create streamlit_app.py

--- Create streamlit_app.py
+++ Create streamlit_app.py
@@ -17,3 +17,9 @@ streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 my_fruit_list = my_fruit_list.set_index('Fruit')
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
+
+streamlit.header("Fruityvice Fruit Advice!")
